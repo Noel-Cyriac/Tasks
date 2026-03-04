8a711b4cb2dae968bf95453e0aab250630cbacb1
Noel Cyriac <noel.cyriac@innovaturelabs.com>
2026-03-04 15:37:19 +0530

    auth feature added

---
+++ backend-assignment/Task2/src/main/java/com/nc/Task2/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nc.Task2.repository;
+
+import com.nc.Task2.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String name);
+}
